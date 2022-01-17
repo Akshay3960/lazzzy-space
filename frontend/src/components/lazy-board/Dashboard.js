dd5fae1790b344edee2fe21b7c1fba4cbb30c400
Ron Jacob <generalsmoke23@gmail.com>
2022-01-17 22:17:35 +0530

    complete dashboard

--- frontend/src/components/lazy-board/Dashboard.js
+++ frontend/src/components/lazy-board/Dashboard.js
@@ -1,8 +1,13 @@
+import { useRef, useState } from "react";
+
 import styles from "./Dashboard.module.css";
 import Tasks from "../tasks/Tasks";
 
 const Dashboard = () => {
-  const DUMMY_TASKS = [
+  const taskListInputRef = useRef();
+  const [enterTaskList, setEnterTaskList] = useState(false);
+
+  const [taskList, setTaskList] = useState([
     {
       id: "t1",
       name: "Resources",
@@ -23,13 +28,64 @@ const Dashboard = () => {
       name: "Exams",
       tasks: ["PPL exam", "distributed System", "Computer Security"],
     },
-  ];
+  ]);
 
-
-  const dashboard = DUMMY_TASKS.map((item) => (
+  const dashboard = taskList.map((item) => (
     <Tasks key={item.id} id={item.id} title={item.name} tasks={item.tasks} />
   ));
-  return <div className={styles["dashboard"]}>{dashboard}</div>;
+
+  const openAddTaskListHandler = () => {
+    setEnterTaskList(true);
+  };
+  const closeAddTaskListHandler = () => {
+    setEnterTaskList(false);
+  };
+
+  const addTaskListHandler = () => {
+    setTaskList((prevState) => {
+      return [
+        ...prevState,
+        {
+          id: Math.random(),
+          name: taskListInputRef.current.value,
+          tasks: [],
+        },
+      ];
+    });
+    setEnterTaskList(false);
+  };
+
+  const EnterTaskListForm = () => {
+    return (
+      <div className={styles["form-content"]}>
+        <div className={styles["form-control"]}>
+          <label htmlFor="value">Enter Value:</label>
+          <input type="text" id="title" ref={taskListInputRef} />
+        </div>
+
+        <div className={styles["form-actions"]}>
+          <button type="button" onClick={closeAddTaskListHandler}>
+            Close
+          </button>
+          <button onClick={addTaskListHandler}> Save </button>
+        </div>
+      </div>
+    );
+  };
+  return (
+    <div className={styles["dashboard"]}>
+      {dashboard}
+      {enterTaskList && <EnterTaskListForm />}
+      {!enterTaskList && (
+        <button
+          className={styles["dash-button"]}
+          onClick={openAddTaskListHandler}
+        >
+          Add another list
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default Dashboard;
