c67c6f44d9e5f3aeab5cdafaa4546067c9ee3a77
ananthanandanan <ananthanandanan@gmail.com>
2022-02-11 18:12:55 +0530

    Add setFav api

--- frontend/src/components/lazy-board/BoardBar.js
+++ frontend/src/components/lazy-board/BoardBar.js
@@ -1,22 +1,46 @@
+import { useContext } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { BsFillStarFill, BsStar } from "react-icons/bs";
 
 import styles from "./BoardBar.module.css";
 import { boardActions } from "../../store/board-slice";
+import AuthContext from "../../store/auth-context";
+import axios from "axios";
 
 const BoardBar = (props) => {
+  const authCtx = useContext(AuthContext);
   const dispatch = useDispatch();
   const title = useSelector((state) => state.board.title);
   const boardId = useSelector((state) => state.board._id);
   const isFavorite = useSelector((state) => state.board.isFavorite);
 
-  const toggleFavoritesHandler = () => {
+  const toggleFavoritesHandler = async() => {
     if (isFavorite) {
       props.onOthers(boardId);
     } else {
       props.onFavorite(boardId);
     }
     dispatch(boardActions.toggleFavorites());
+    
+    const BACKEND_URL = process.env.REACT_APP_API_URL;
+    const user_id = authCtx._id;
+
+    const data = {
+      isFavourite: isFavorite
+    }
+    try{
+      await axios.put(
+        BACKEND_URL + 
+        'api/users/setfav/' + 
+        user_id +
+        '/' + 
+        boardId
+        , data);
+      
+    }
+    catch(err){
+      console.log(err);
+    }
   };
 
   return (
